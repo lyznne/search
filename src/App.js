05fe21ca93f8d7be7d19147d0798e8ce1eb77810
lyznne <emuthiani26@gmail.com>
2023-08-10 23:29:59 +0300

    1st commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+       <h1 className="text-3xl text-blue text-blue-900 font-bold ">
+      Hello world!
+    </h1>
+    </div>
+  );
+}
+
+export default App;
