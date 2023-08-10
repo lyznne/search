adbb5bb4342fc08f9ac3f870297ae732078eb252
lyznne <emuthiani26@gmail.com>
2023-08-11 00:12:23 +0300

    0.0.1

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-       <h1 className="text-3xl text-blue text-blue-900 font-bold ">
-      Hello world!
-    </h1>
+      <h2>hi there </h2>
     </div>
   );
 }
