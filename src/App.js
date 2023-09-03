56f322b176defaaeb0d1fcf3b34010caae499d9b
lyznne <emuthiani26@gmail.com>
2023-09-03 19:56:59 +0300

    :tada: new

--- src/App.js
+++ src/App.js
@@ -9,3 +9,28 @@ function App() {
 }
 
 export default App;
+
+// urban dictionary
+
+
+
+
+
+const axios = require('axios');
+
+const options = {
+  method: 'GET',
+  url: 'https://mashape-community-urban-dictionary.p.rapidapi.com/define',
+  params: {term: 'wat'},
+  headers: {
+    'X-RapidAPI-Key': 'c4f8d7c594mshb9012d82c8bef86p142dedjsn8226a1e0c010',
+    'X-RapidAPI-Host': 'mashape-community-urban-dictionary.p.rapidapi.com'
+  }
+};
+
+try {
+	const response = await axios.request(options);
+	console.log(response.data);
+} catch (error) {
+	console.error(error);
+}
